a2f478165d8e900fcdc7e6d18d18b9df010f03df
Mark Cunningham <markcham@ou.edu>
2023-11-17 00:34:37 +0000

    Merge working ordering variants

--- noifstatementvarJPI.c
+++ noifstatementvarJPI.c
@@ -95,16 +95,24 @@ void COMPUTE_NAME(int m0, int n0, float *A_distributed, float *B_distributed, fl
 
 	if (rid == root_rid)
 	{
+		/* Initialize with 0 because the initial value will be random garbage,
+		   necessary because using += operator in the i0 loop */
+		for (int i0 = 0; i0 < n0; ++i0)
+		{
+			for (int p0 = 0; p0 < m0; ++p0)
+			{
+				C_distributed[i0 * rs_C + p0] = 0.0f;
+			}
+		}
 		for (int j0 = 0; j0 < n0; ++j0)
 		{
 			for (int p0 = 0; p0 < m0; ++p0)
 			{
+				float B_pj = B_distributed[p0 * cs_B + j0 * rs_B];
 				for (int i0 = 0; i0 < j0; ++i0)
 				{
-					float A_ip = A_distributed[i0 + p0 * rs_A];
-					float B_pj = B_distributed[p0 + j0 * rs_B];
-					// Swap row stride and column stride
-					C_distributed[i0 * rs_C + j0] = A_ip * B_pj;
+					float A_ip = A_distributed[i0 * cs_A + p0 * rs_A];
+					C_distributed[i0 * cs_C + j0 * rs_C] += A_ip * B_pj;
 				}
 			}
 		}
